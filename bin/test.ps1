5b7f69b884d4a9e9dc2601a85f4fd43d88b9bd0e
MaoKwen <18361388+MaoKwen@users.noreply.github.com>
2020-11-14 20:03:36 +0800

    update bin

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,8 +1,4 @@
-<#
-.SYNOPSIS
-    Execute Pester tests in repository root directory.
-#>
+#requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
 
-$result = Invoke-Pester "$PSScriptRoot\..\Scoop-Bucket.Tests.Local.ps1" -PassThru
-
-exit $result.FailedCount
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
+Invoke-Pester "$psscriptroot/.."
