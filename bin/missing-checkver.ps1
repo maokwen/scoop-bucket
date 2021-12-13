948de48c7f3078b3a8dc91f953e49c5840054309
MaoKwen <18361388+MaoKwen@users.noreply.github.com>
2021-12-14 07:38:48 +0800

    Update bin

--- bin/missing-checkver.ps1
+++ bin/missing-checkver.ps1
@@ -1,4 +1,4 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-$missing_checkver = "$env:SCOOP_HOME/bin/missing-checkver.ps1"
-$dir = "$psscriptroot/../bucket" # checks the parent dir
-Invoke-Expression -command "& '$missing_checkver' -dir '$dir' $($args | ForEach-Object { "$_ " })"
+#requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
+
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+Invoke-Pester "$psscriptroot/.."
